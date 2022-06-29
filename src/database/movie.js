d72b2d1c14b8d16592881470985791a607c9d721
Hanbyeol Shin /  David Shin / 신한별 <76985229+shinhanbyeol@users.noreply.github.com>
2022-06-29 11:05:10 +0900

    Fix issue #2

--- src/database/movie.js
+++ src/database/movie.js
@@ -10,11 +10,47 @@ export const movies = async (args) => {
     `match(n: Movie ${argsCypher})-[e]-(m) return n,e,m;`
   );
   client.end();
-  return result.rows.map((data) => {    
-    const graphObj = Object({ ...data.n.props });
-    graphObj[`${data.e.label}`] = [{...data.m.props}]
-    return graphObj;
+  const matchRotationList = [];
+  const edgeStack = {};
+  const cypherlResultList = [];
+  
+  result.rows.forEach((data) => {
+    let graphObj;
+    const oid = `${data.n.id.oid}.${data.n.id.id}`;
+    if (matchRotationList.indexOf(oid) === -1) {
+      graphObj = Object({ gid: oid, ...data.n.props });
+      if (data.e) {
+        const edgelabelAndProps = {};
+        edgelabelAndProps[`${data.e.label}`] = [{ ...data.m.props }];
+        edgeStack[oid] = edgelabelAndProps;
+      }
+    } else {
+      if (data.e) {
+        if (edgeStack[oid][`${data.e.label}`]) {
+          edgeStack[oid][`${data.e.label}`].push(data.m.props);
+        } else {
+          const edgelabelAndProps = {};
+          edgelabelAndProps[`${data.e.label}`] = [{ ...data.m.props }];
+          edgeStack[oid] = edgelabelAndProps;
+        }
+      }
+    }
+    matchRotationList.push(oid);
+    if (graphObj) cypherlResultList.push(graphObj);
   });
+
+  const graphqlResponse = cypherlResultList.map((graphObj) => {
+    let gData;
+    const edgeInfo = edgeStack[graphObj.gid];    
+    delete graphObj.gid;
+    gData = {
+      ...graphObj,
+      ...edgeInfo,
+    }
+    return gData;
+  });
+
+  return graphqlResponse;
 };
 
 export const movie = async (args) => {
